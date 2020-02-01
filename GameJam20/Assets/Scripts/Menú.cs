57eb04a25049d2417dfaf23a9e95d2ae9505cb42
Rubb-hub <ruben.bastante.ro@gmsil.com>
2020-02-01 11:06:04 +0100

    Unity_Add

---
+++ GameJam20/Assets/Scripts/Menú.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menú : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void New_Game()
+    {
+        SceneManager.LoadScene("Niveles");
+    }
+}
---
+++ GameJam20/Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D PlayerRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movement = Input.GetAxis("Horizontal");
+        PlayerRB.velocity = new Vector2(movement * speed, PlayerRB.velocity.y);
+    }
+}
